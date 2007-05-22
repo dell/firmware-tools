9ce1d496a8de4ee53b08705cf6a2a4081c40951c
Michael E Brown <michael_e_brown@dell.com>
2007-05-22 16:12:06 -0500

    add optional user-name to autobuild hook search, as well as fallback to generic.

--- pkg/autobuild-hook.sh
+++ pkg/autobuild-hook.sh
@@ -3,6 +3,10 @@
 CURDIR=$(cd $(dirname $0); pwd)
 
 [ -z "$BUILD_CYCLE" ] || export RELEASE_EXTRALEVEL=.${BUILD_CYCLE}.autobuild
-if [ -x $CURDIR/ab-host-$(hostname).sh ]; then
+if [ -x $CURDIR/ab-host-$(hostname)-$(id -un).sh ]; then
+    $CURDIR/ab-host-$(hostname)-$(id -un).sh
+elif [ -x $CURDIR/ab-host-$(hostname).sh ]; then
     $CURDIR/ab-host-$(hostname).sh
+elif [ -x $CURDIR/ab-generic.sh ]; then
+    $CURDIR/ab-generic.sh
 fi
