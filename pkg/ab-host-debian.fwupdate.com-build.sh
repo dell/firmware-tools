1f39e86bf967ea8f29007145e71f0e92be2903b5
Michael E Brown <michael_e_brown@dell.com>
2007-05-24 14:35:05 -0500

    dont override version passed in by autobuilder.

--- pkg/ab-host-debian.fwupdate.com-build.sh
+++ pkg/ab-host-debian.fwupdate.com-build.sh
@@ -11,14 +11,10 @@ umask 002
 [ -n "$APT_REPO" ] || 
     APT_REPO=/var/ftp/pub/yum/dell-repo/testing/debian/incoming
 
-. version.mk
-RELEASE_VERSION=${RELEASE_MAJOR}.${RELEASE_MINOR}.${RELEASE_SUBLEVEL}${RELEASE_EXTRALEVEL}
-RELEASE_STRING=${RELEASE_NAME}-${RELEASE_VERSION}
-
 set -e
 
 make distclean
-make deb
+make -e deb
 
 # need to port the following to pbuilder
 mkdir -p ${APT_REPO}/etch-i386/${RELEASE_NAME}/${RELEASE_VERSION}-${DEB_RELEASE}/
