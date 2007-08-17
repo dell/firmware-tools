9a7c53fbe056787bbdf7627de98b530a71470e5e
Michael E Brown <michael_e_brown@dell.com>
2007-08-17 14:17:24 -0500

    add common helpers for gui tasks

---
+++ firmwaretools/guihelpers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# vim:ai:ts=4:sw=4:et:filetype=python:
+
+#future imports always first
+from __future__ import generators
+
+# std python stuff
+import gtk
+
+def getSelectionPaths(treeview):
+    def func(model, path, iterator, data):
+        model = None
+        iterator = None
+        data.append(path)
+
+    paths = []
+    treeselection = treeview.get_selection()
+    treeselection.selected_foreach(func, paths)
+    return paths
+
+def gtkYield():
+    # process gui events during long-running loops
+    # so that we are more responsive
+    while gtk.events_pending():                        
+        gtk.main_iteration(False)
+
+
+
