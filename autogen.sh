7eb79a56cc3b9eee94f66e35e182b3f108d04151
Michael E Brown <michael_e_brown@dell.com>
2008-01-10 13:40:29 -0600

    have autogen run configure, but also make it compatible with blddir!=srcdir

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -e
 
 # run this script to create all the autotools fluff
 
-set -e
+DIR=$(cd $(dirname $0); pwd)
+pushd $DIR
+
 aclocal
 automake --force --foreign --add-missing -c
 autoconf --force
+
+popd
+
+$DIR/configure
