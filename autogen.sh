b9b3df1f56e1bf92daaa135ca1ed615e012b832d
Michael E Brown <michael_e_brown@dell.com>
2008-01-10 13:42:31 -0600

    Revert "have autogen run configure, but also make it compatible with blddir!=srcdir"
    This reverts commit 7eb79a56cc3b9eee94f66e35e182b3f108d04151.
    
    Not a good idea after all.

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-set -e
 
 # run this script to create all the autotools fluff
 
-DIR=$(cd $(dirname $0); pwd)
-pushd $DIR
-
+set -e
 aclocal
 automake --force --foreign --add-missing -c
 autoconf --force
-
-popd
-
-$DIR/configure
