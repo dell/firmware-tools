7e22c616bfd22efe7b2deab22f6d9c0146f17ccd
Michael E Brown <michael_e_brown@dell.com>
2008-02-02 16:25:16 -0600

    fixup copyright headers and attributions.

--- ft-cli/ftcommands.py
+++ ft-cli/ftcommands.py
@@ -12,6 +12,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Copyright (C) 2008 Dell Inc.
+#  by Michael Brown <Michael_E_Brown@dell.com>
+#
+# Based in part on design and code in Yum 3.2:
 # Copyright 2006 Duke University 
 # Written by Seth Vidal
 
