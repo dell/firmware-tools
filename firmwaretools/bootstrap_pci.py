94d5a0c78b417bc92482bc6e6b6ecf60279092e4
Michael E Brown <mebrown@michaels-house.net>
2008-02-10 13:40:47 -0600

    rewrite bootstrap pci to use sysfs rather than hacky lspci.

--- firmwaretools/bootstrap_pci.py
+++ firmwaretools/bootstrap_pci.py
@@ -32,54 +32,52 @@ requires_api_version = "2.0"
 # ======
 
 def config_hook(conduit, *args, **kargs):
-    conduit.getBase().registerBootstrapFunction( "bootstrap_pci", BootstrapGenerator )
+    conduit.getBase().registerBootstrapFunction( "bootstrap_pci", getPciDevs )
 
 
-decorate(traceLog())
-def BootstrapGenerator(*args, **kargs):
-    for i in lspciGenerator():
-        yield(makePciDevice(i))
-
-# ======
-# private stuff from here down
-# ======
-
-vendevRe = re.compile(r'^(.*)\[(\w+)\]')
+sysfs_pcidevdir="/sys/bus/pci/devices"
 
 decorate(traceLog())
-def splitTextFromNumeric(line):
-    match = vendevRe.search(line)
-    if match:
-        text = match.group(1)
-        number = match.group(2)
-        return (text, number)
-    return None
+def getPciDevs(devdir=sysfs_pcidevdir, *args, **kargs):
+    for d in os.listdir(devdir):
+        yield makePciDevice(os.path.join(devdir, d))
 
 decorate(traceLog())
-def makePciDevice(oneDevData):
+def getFile(f):
+    fd = open(f,"r")
+    ret = fd.read()
+    fd.close()
+    if ret[-1:] == '\n': ret = ret[:-1]
+    return ret
+
+decorate(traceLog())
+def makePciDevice(devDir):
     kargs = {}
+    kargs["pciVendor"] = int(getFile(os.path.join(devDir, "vendor")),16)
+    kargs["pciDevice"] = int(getFile(os.path.join(devDir, "device")),16)
+    kargs["pciSubVendor"] = int(getFile(os.path.join(devDir, "subsystem_vendor")),16)
+    kargs["pciSubDevice"] = int(getFile(os.path.join(devDir, "subsystem_device")),16)
+    kargs["pciClass"] = int(getFile(os.path.join(devDir, "class")),16)
 
-    kargs["pciVendor_txt"], kargs["pciVendor"] = splitTextFromNumeric(oneDevData["vendor"])
-    kargs["pciDevice_txt"], kargs["pciDevice"] = splitTextFromNumeric(oneDevData["device1"])
-    name = "pci_firmware(ven_0x%s_dev_0x%s" % (kargs["pciVendor"], kargs["pciDevice"])
-    if oneDevData.has_key("svendor"):
-        kargs["pciSubVendor_txt"], kargs["pciSubVendor"] = splitTextFromNumeric(oneDevData["svendor"])
-        kargs["pciSubDevice_txt"], kargs["pciSubDevice"] = splitTextFromNumeric(oneDevData["sdevice"])
-        name = name + "_subven_0x%s_subdev_0x%s" % (kargs["pciSubVendor"], kargs["pciSubDevice"])
+    name = "pci_firmware(ven_0x%x_dev_0x%x" % (kargs["pciVendor"], kargs["pciDevice"])
+    if kargs["pciSubVendor"] and kargs["pciSubDevice"]:
+        name = name + "_subven_0x%x_subdev_0x%x" % (kargs["pciSubVendor"], kargs["pciSubDevice"])
     name = name + ")"
 
-    displayname = "%s %s" % (kargs["pciVendor_txt"], kargs["pciDevice_txt"])
-    if kargs.has_key("pciSubVendor_txt"):
-        if (not kargs["pciSubVendor_txt"].lower().startswith("unknown") and
-                not kargs["pciSubDevice_txt"].lower().startswith("unknown")):
-            displayname = "%s %s" % (kargs["pciSubVendor_txt"], kargs["pciSubDevice_txt"])
+    dirname = os.path.basename(devDir)
+    dets = dirname.split(":")
+    kargs["pciBDF_Domain"] = int(dets[0],16)
+    kargs["pciBDF_Bus"] = int(dets[1],16)
+    kargs["pciBDF_Device"] = int(dets[2].split(".")[0],16)
+    kargs["pciBDF_Function"] = int(dets[2].split(".")[1],16)
 
-    kargs["pciBDF_Domain"], kargs["pciBDF_Bus"], devfun = oneDevData["device0"].split(":")
-    kargs["pciBDF_Domain"] = int(kargs["pciBDF_Domain"],16)
-    kargs["pciBDF_Bus"] = int(kargs["pciBDF_Bus"],16)
-    kargs["pciBDF_Device"], kargs["pciBDF_Function"] = [int(i,16) for i in devfun.split(".")]
-    kargs["pciClass"] = splitTextFromNumeric(oneDevData["class"])[1]
     kargs["pciDbdf"] = (kargs["pciBDF_Domain"], kargs["pciBDF_Bus"], kargs["pciBDF_Device"], kargs["pciBDF_Function"])
+
+    kargs["pciVendor_txt"] = "unknown vendor"
+    kargs["pciDevice_txt"] = "unknown device"
+    kargs["pciSubVendor_txt"] = ""
+    kargs["pciSubDevice_txt"] = ""
+    displayname = "%s %s" % (kargs["pciVendor_txt"], kargs["pciDevice_txt"])
 
     return package.PciDevice(
         name=name,
@@ -88,102 +86,7 @@ def makePciDevice(oneDevData):
         **kargs
         )
 
-# overridden by unit test code.
-mockReadLspciWithDomain = os.popen
-mockReadLspciWithoutDomain = os.popen
-
-lspciPath=None
-for i in ("/sbin/lspci", "/usr/bin/lspci"):
-    if os.path.exists(i):
-        lspciPath=i
-        break
-
-decorate(traceLog())
-def lspciGenerator():
-    oneDevData = {}
-
-    fd = mockReadLspciWithDomain("%s -nn -m -v -D 2>/dev/null" % lspciPath, "r")
-    deviceNum=0
-    for line in fd:
-        if not line.strip():
-            deviceNum=0
-            yield oneDevData
-            oneDevData = {}
-            continue
-
-        name,value = line.split(":", 1)
-        name = name.strip().lower()
-        value = value.strip()
-        if name == "device":
-            name = "device%s" % deviceNum
-            deviceNum = deviceNum + 1
-        oneDevData[name] = value
-
-    try:
-        err = fd.close()
-    except AttributeError:
-        err = False
-
-    if err:
-        # Do it the hard way
-
-        fd = mockReadLspciWithoutDomain("%s -nn -m -v 2>/dev/null" % lspciPath, "r")
-        deviceNum=0
-        for line in fd:
-            if not line.strip():
-                deviceNum=0
-                yield supplementOldLspciFormat(oneDevData)
-                oneDevData = {}
-                continue
-
-            name,value = line.split(":", 1)
-            name = name.strip().lower()
-            value = value.strip()
-            if name == ("device"):
-                name = "device%s" % deviceNum
-                deviceNum = deviceNum + 1
-            oneDevData[name] = value
-        fd.close()
-
-decorate(traceLog())
-def supplementOldLspciFormat(oneDevData):
-    #oneDevData["device0"]
-    #oneDevData["vendor"] = "old-lspci-format-fixme [%s]" % oneDevData["vendor"]
-    #oneDevData["device1"] = "old-lspci-format-fixme [%s]" % oneDevData["device1"]
-    #if oneDevData.has_key("svendor"): oneDevData["svendor"] = "old-lspci-format-fixme [%s]" % oneDevData["svendor"]
-    #if oneDevData.has_key("sdevice"): oneDevData["sdevice"] = "old-lspci-format-fixme [%s]" % oneDevData["sdevice"]
-
-    fd = os.popen("%s -m -v -s %s 2>/dev/null" % (lspciPath, oneDevData["device0"]), "r")
-    if len(oneDevData["device0"].split(":")) < 3:
-        oneDevData["device0"] = "0000:" + oneDevData["device0"].strip()
-
-    deviceNum=0
-    for line in fd:
-        if not line.strip(): continue
-        name,value = line.split(":", 1)
-        name = name.strip().lower()
-        value = value.strip()
-        if name == ("device"):
-            name = "device%s" % deviceNum
-            deviceNum = deviceNum + 1
-
-        if name == "class":
-            oneDevData["class"] = "%s [%s]" % (value, oneDevData["class"].split()[-1])
-
-        if name == "vendor":
-            oneDevData["vendor"] = "%s [%s]" % (value, oneDevData["vendor"])
-        if name == "svendor":
-            oneDevData["svendor"] = "%s [%s]" % (value, oneDevData["svendor"])
-        if name == "device1":
-            oneDevData["device1"] = "%s [%s]" % (value, oneDevData["device1"])
-        if name == "sdevice":
-            oneDevData["sdevice"] = "%s [%s]" % (value, oneDevData["sdevice"])
-
-    fd.close()
-
-    return oneDevData
-
 if __name__ == "__main__":
-    for pkg in BootstrapGenerator():
-        print "%s: %s" % (pkg.name, str(pkg))
+    for p in getPciDevs():
+        print "%s" % p.name
 
