782facfbdf0b62f77ad83889193afd3740939bb7
Michael E Brown <michael_e_brown@dell.com>
2008-03-04 08:59:01 -0600

    make sure spinprint doesnt scroll on default terminal sizes.

--- firmwaretools/pycompat.py
+++ firmwaretools/pycompat.py
@@ -40,9 +40,16 @@ def spinner(cycle=['/', '-', '\\', '|']):
     # ESC codes for clear line and position cursor at horizontal pos 0
     return step
 
+def pad(strn, pad_width=67):
+    # truncate strn to pad_width so spinPrint does not scroll
+    if len(strn) > pad_width:
+        return strn[:pad_width] + ' ...'
+    else:
+        return strn
+
 def spinPrint(strn, outFd=sys.stderr):
     outFd.write(clearLine())
-    outFd.write("%s\t%s" % (spinner(), strn))
+    outFd.write("%s\t%s" % (spinner(), pad(strn)))
     outFd.flush()
 
 def timedSpinPrint( strn, start ):
