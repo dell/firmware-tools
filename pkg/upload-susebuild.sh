96ef7523dd75c234f0537943654b2f8f044c47d4
Michael E Brown <michael_e_brown@dell.com>
2008-08-19 17:33:47 -0500

    add script to upload to opensuse build service

---
+++ pkg/upload-susebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -x
+
+PROJECT=$1
+PACKAGE=$2
+if [ -z "$PROJECT" -o -z "$PACKAGE" ]; then
+    echo "Must specify project and package"
+    exit 1
+fi
+
+osc co $PROJECT $PACKAGE
+rm $PROJECT/$PACKAGE/*.tar.gz
+rm $PROJECT/$PACKAGE/*.spec
+cp ${PACKAGE}*.tar.gz $PROJECT/$PACKAGE
+cp */${PACKAGE}.spec $PROJECT/$PACKAGE
+pushd $PROJECT/$PACKAGE
+osc addremove
+osc ci -m "scripted source update"
+popd
