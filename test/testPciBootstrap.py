2b550f46e9a49fccd6c9502a17a228a984939bc0
Jeffrey L Mendoza <Jeffrey_L_Mendoza@Dell.com>
2009-06-30 19:50:28 -0500

    Fixed testcase that broke on SLES11 with last commit.
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

--- test/testPciBootstrap.py
+++ test/testPciBootstrap.py
@@ -8,6 +8,7 @@ from __future__ import generators
 import sys
 import unittest
 import ConfigParser
+import os
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -32,7 +33,13 @@ class TestCase(unittest.TestCase):
             plugins.TYPE_CORE,
             plugins.TYPE_MOCK_CORE, plugins.TYPE_MOCK_INVENTORY,
             ] 
-        f._getConfig(None, pluginTypes, None, [])
+        #instead of "f._getConfig(None, pluginTypes, None, [])"
+        # do the below to avoid "logging.config.fileConfig(configFile)"
+        cfgFiles = [os.path.join(firmwaretools.PKGCONFDIR, "firmware.conf"),]
+        f.conf = firmwaretools.confObj()
+        f.setConfFromIni(cfgFiles)
+        f.conf.uid = os.geteuid()
+        f.doPluginSetup(None , pluginTypes, [])
 
         # import functions for bootstrap/compare
 
